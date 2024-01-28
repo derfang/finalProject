95bc23f1867bca95023b146b00a1cfec540c6945
derfang <erfanps6@gmail.com>
2024-01-28 18:02:41 +0330

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PADIDAR/Desktop/temp/codes/cpp/finalProject
+# Build directory: C:/Users/PADIDAR/Desktop/temp/codes/cpp/finalProject/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
